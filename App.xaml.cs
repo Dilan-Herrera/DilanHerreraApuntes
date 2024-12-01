519d4c72a98bde2de1db64104f62d4da49cb4d23
Dilan-Herrera <dilan.herrera@udla.edu.ec>
2024-12-01 17:34:54 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DilanHerreraApuntes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
